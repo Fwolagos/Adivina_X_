0937c1e5edc336b4a45b9080336251644f96f43e
Fwolagos <aantonypv@gmail.com>
2024-01-27 19:31:45 -0600

    Se programa los todas la funciones del form de lista de intentos del usuarios, y los botones

--- ListaIntentos.Designer.cs
+++ ListaIntentos.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListaIntentos));
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -40,16 +42,39 @@
             this.listBox1.Size = new System.Drawing.Size(120, 277);
             this.listBox1.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Volver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cargar Lista";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // ListaIntentos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(203, 294);
+            this.ClientSize = new System.Drawing.Size(249, 294);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "ListaIntentos";
             this.Text = "ListaIntentos";
+            this.Load += new System.EventHandler(this.ListaIntentos_Load);
             this.ResumeLayout(false);
 
         }
@@ -57,5 +82,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- ListaIntentos.cs
+++ ListaIntentos.cs
@@ -16,5 +16,38 @@ namespace Adivina_X_
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Muestra el form 1 al salir o cerrar el program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ListaIntentos_Load(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+        /// <summary>
+        /// Boton para volver a la pagina Principal.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+        /// <summary>
+        /// Carga la lista de los numero probados por el jugador
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(Program.tempListNumUsert.Cast<object>().ToArray());
+        }
     }
+    
 }
